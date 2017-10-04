a4bfce0a173d19dc7e17bcbce641663921113d83
胡天水 <agebull@QQ.com>
2017-10-04 23:22:41 +0800

    项目文件名规范

---
+++ src/CodeTemplate/LuaContext.cs
@@ -0,0 +1,16 @@
+﻿using Agebull.EntityModel.Config;
+
+namespace Agebull.EntityModel.RobotCoder.CodeTemplate.LuaTemplate
+{
+    public class LuaContext : CoderBase
+    {
+        public EntityConfig Entity { get; set; }
+        public EntityConfig Project { get; set; }
+        public EntityConfig Soluction { get; set; }
+
+        public EntityConfig GetEntity()
+        {
+            return Entity;
+        }
+    }
+}
