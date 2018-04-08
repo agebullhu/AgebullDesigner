b38628abeeebdf54188c4e70a16217aa2ffa92ac
agebullhu <agebull@qq.com>
2018-04-08 14:41:27 +0800

    **合并代码**

---
+++ src/Coder.Project/Cpp/EsFundsProjectBuilde.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.Composition;
+using Agebull.EntityModel.Config;
+using Agebull.EntityModel.Designer;
+
+namespace Agebull.EntityModel.RobotCoder.Cpp
+{
+    [Export(typeof(IAutoRegister))]
+    [ExportMetadata("Symbol", '%')]
+    internal sealed class EsFundsProjectBuilde : ProjectBuilder, IAutoRegister
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        protected override string Name => "EsFunds";
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public override string Caption => "易盛期货接口代码";
+        /// <summary>
+        /// 执行自动注册
+        /// </summary>
+        void IAutoRegister.AutoRegist()
+        {
+            RegistBuilder<EsFundsProjectBuilde>();
+        }
+        /// <summary>
+        /// 生成项目代码
+        /// </summary>
+        /// <param name="project"></param>
+        public override void CreateProjectCode(ProjectConfig project)
+        {
+            var coder = new EsPublishCoder
+            {
+                Project = project
+            };
+            coder.CreateBaseCode(null);
+        }
+
+        /// <summary>
+        /// 生成实体代码
+        /// </summary>
+        /// <param name="project"></param>
+        /// <param name="schema"></param>
+        public override void CreateEntityCode(ProjectConfig project, EntityConfig schema)
+        {
+        }
+    }
+}
